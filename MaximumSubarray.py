e2dcda79d0208f6ad906df3bf4070aab2687bd36
Anastasiya <aadworkmaill@gmail.com>
2026-01-17 20:03:50 +0300

    Create MaximumSubarray.py

---
+++ MaximumSubarray.py
@@ -0,0 +1,37 @@
+'''
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+'''
+
+
+def maxSubArray(nums):
+
+    current_max = nums[0]
+    global_max = nums[0]
+
+    for num in nums[1:]:
+
+        current_max = max(num, num+current_max)
+
+        if current_max > global_max:
+
+            global_max = current_max
+
+    return global_max
